f96990f971d954e63b59e57f483a48cd29c29ad1
Hadi <mhn.hadi@outlook.com>
2025-09-26 08:00:29 +0330

    Added on-screen drawing to the script.

--- scripts/toggle_drawing_mode.sh
+++ scripts/toggle_drawing_mode.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+kill_drawing_options() {
+	kill -9 $(ps aux | grep config-pen | sed 1q | tr -s " " | cut -f2 -d" ")
+	for var in $(ps aux | grep -i Gromit-mpx | sed 3q | tr -s " " | cut -f2 -d" "); do
+		kill -9 $var
+	done
+}
+
+run_drawing_options() {
+	flatpak run net.christianbeier.Gromit-MPX &
+	waybar -c $HOME/.config/waybar/config-pen.jsonc &
+}
+
 [ $(ps aux | grep config-pen | wc -l) -gt 1 ] \
-	&& kill -9 $(ps aux | grep config-pen | sed 1q | tr -s " " | cut -f2 -d" ") \
-	|| waybar -c $HOME/.config/waybar/config-pen.jsonc &
+	&& kill_drawing_options \
+	|| run_drawing_options
+
